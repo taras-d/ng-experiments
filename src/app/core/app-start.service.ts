15781e375d057eb3b7629747c7ed3715e0a91f56
taras-d <taras.datsenko@gmail.com>
2018-03-28 20:45:43 +0300

    Format code

--- src/app/core/app-start.service.ts
+++ src/app/core/app-start.service.ts
@@ -3,16 +3,16 @@ import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs/Observable';
 
 import { ApiService } from './api.service';
- 
+
 @Injectable()
 export class AppStartService {
 
-    constructor(private apiService: ApiService) {
+  constructor(private apiService: ApiService) {
 
-    }
+  }
 
-    load(): Promise<any> {
-        return Observable.of(null).toPromise();
-    }
+  load(): Promise<any> {
+    return Observable.of(null).toPromise();
+  }
 
 }
--- src/app/core/utils/observableManager.ts
+++ src/app/core/utils/observableManager.ts
@@ -2,73 +2,73 @@ import { Observable } from 'rxjs/Observable';
 import { Subscription } from 'rxjs/Subscription';
 
 export interface ObserverConfig {
-    create: (...args: any[]) => Observable<any>;
-    next?: (data: any, ...args: any[]) => void;
-    error?: (error: any, ...args: any[]) => void;
-    complete?: (args: any[]) => void;
+  create: (...args: any[]) => Observable<any>;
+  next?: (data: any, ...args: any[]) => void;
+  error?: (error: any, ...args: any[]) => void;
+  complete?: (args: any[]) => void;
 }
 
 export interface ObserversConfig {
-    [key: string]: ObserverConfig;
+  [key: string]: ObserverConfig;
 }
 
 export interface ObservableManagerOptions {
-    next?: (name: string, data: any, ...args: any[]) => void;
-    error?: (name: string, error: any, ...args: any[]) => void;
-    complete?: (name: string, ...args: any[]) => void;
+  next?: (name: string, data: any, ...args: any[]) => void;
+  error?: (name: string, error: any, ...args: any[]) => void;
+  complete?: (name: string, ...args: any[]) => void;
 }
 
 export class ObservableManager {
 
-    private subs: Subscription[] = [];
+  private subs: Subscription[] = [];
 
-    constructor(
-        private config: ObserversConfig, 
-        private options?: ObservableManagerOptions
-    ) {}
+  constructor(
+      private config: ObserversConfig, 
+      private options?: ObservableManagerOptions
+  ) {}
 
-    exec(name: string, ...args: any[]): void {
-        const config = this.config[name];
+  exec(name: string, ...args: any[]): void {
+    const config = this.config[name];
 
-        if (!config) {
-            console.warn(`Observer with name "${name}" not found`);
-            return;
-        }
-
-        this.unsub(name);
-
-        const options = this.options || {};
-
-        this.subs[name] = config.create(...args).subscribe(
-            data => {
-                this.callFn(config.next, data, ...args);
-                this.callFn(options.next, name, data, ...args);
-            },
-            error => {
-                this.callFn(config.error, error, ...args);
-                this.callFn(options.error, name, error, ...args);
-            },
-            () => {
-                this.callFn(config.complete, ...args);
-                this.callFn(options.complete, name, ...args);
-            }
-        );
+    if (!config) {
+      console.warn(`Observer with name "${name}" not found`);
+      return;
     }
 
-    unsub(name: string): void {
-        if (name in this.subs) {
-            this.subs[name].unsubscribe();
-        }
-    }
+    this.unsub(name);
 
-    unsubAll(): void {
-        Object.keys(this.config).forEach(name => this.unsub(name));
-    }
+    const options = this.options || {};
 
-    private callFn(fn: Function, ...args: any[]): any {
-        if (typeof fn === 'function') {
-            return fn(...args);
-        }
+    this.subs[name] = config.create(...args).subscribe(
+      data => {
+        this.callFn(config.next, data, ...args);
+        this.callFn(options.next, name, data, ...args);
+      },
+      error => {
+        this.callFn(config.error, error, ...args);
+        this.callFn(options.error, name, error, ...args);
+      },
+      () => {
+        this.callFn(config.complete, ...args);
+        this.callFn(options.complete, name, ...args);
+      }
+    );
+  }
+
+  unsub(name: string): void {
+    if (name in this.subs) {
+      this.subs[name].unsubscribe();
     }
+  }
+
+  unsubAll(): void {
+    Object.keys(this.config).forEach(name => this.unsub(name));
+  }
+
+  private callFn(fn: Function, ...args: any[]): any {
+    if (typeof fn === 'function') {
+      return fn(...args);
+    }
+  }
 
 }
