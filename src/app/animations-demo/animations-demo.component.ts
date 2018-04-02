83ad2ef86a340422621dac230e57ec8912228f50
taras-d <taras.datsenko@gmail.com>
2018-04-02 23:21:54 +0300

    Animations

--- src/app/animations-demo/animations-demo.component.ts
+++ src/app/animations-demo/animations-demo.component.ts
@@ -1,5 +1,16 @@
 import { Component, OnInit } from '@angular/core';
 import { trigger, transition, state, style, animate } from '@angular/animations';
+import { Observable } from 'rxjs/Observable';
+
+const FRUITS = [
+  'Apple',
+  'Banana',
+  'Tangerine',
+  'Grape',
+  'Kiwi'
+];
+
+let ID = 1;
 
 @Component({
   selector: 't-animations-demo',
@@ -10,11 +21,42 @@ import { trigger, transition, state, style, animate } from '@angular/animations'
       state('void', style({ opacity: 0 })),
       state('*', style({ opacity: 1 })),
       transition(':enter, :leave', animate(500))
+    ]),
+    trigger('listItem', [
+      transition(':enter', [
+        style({ transform: 'translateX(-100%)' }),
+        animate(200, style({ transform: 'translateX(0)' }))
+      ]),
+      transition(':leave', [
+        animate(200, style({ transform: 'translateX(100%)' }))
+      ])
     ])
   ]
 })
-export class AnimationsDemoComponent {
+export class AnimationsDemoComponent implements OnInit {
 
   showIcon = false;
+
+  items = [];
+
+  ngOnInit(): void {
+    Observable.interval(300).take(5)
+      .subscribe(() => this.onItemAdd());
+  }
+
+  onItemAdd(): void {
+    this.items.push({
+      id: ID++,
+      name: FRUITS[Math.floor(Math.random() * FRUITS.length)]
+    });
+  }
+
+  onItemClick(index: number): void {
+    this.items.splice(index, 1);
+  }
+
+  trackBy(index: number, item: any): void {
+    return item.id;
+  }
 
 }
--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -4,6 +4,7 @@ import { HttpClientModule } from '@angular/common/http';
 
 import 'rxjs/add/observable/of';
 import 'rxjs/add/observable/throw';
+import 'rxjs/add/observable/interval';
 import 'rxjs/add/operator/map';
 import 'rxjs/add/operator/filter';
 import 'rxjs/add/operator/do';
@@ -11,6 +12,7 @@ import 'rxjs/add/operator/delay';
 import 'rxjs/add/operator/share';
 import 'rxjs/add/operator/toPromise';
 import 'rxjs/add/operator/catch';
+import 'rxjs/add/operator/take';
 
 import { ApiService } from './api.service';
 import { AppStartService } from './app-start.service';
