d285a5f31b8b8475ac3b79aa1b1e9fd76720641e
taras-d <taras.datsenko@gmail.com>
2018-08-10 11:17:27 +0300

    Layout component

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,21 +1,30 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
+
 import { ModalComponent } from './modal/modal.component';
 import { TooltipComponent } from './tooltip/tooltip.component';
 import { LoaderComponent } from './loader/loader.component';
 import { CheckboxComponent } from './checkbox/checkbox.component';
+import { LayoutComponent } from './layout/layout.component';
 
 @NgModule({
   imports: [
-    CommonModule
+    CommonModule,
+    RouterModule
   ],
   exports: [
+    CommonModule,
+    RouterModule,
+
+    LayoutComponent,
     ModalComponent,
     TooltipComponent,
     LoaderComponent,
     CheckboxComponent
   ],
   declarations: [
+    LayoutComponent,
     ModalComponent,
     TooltipComponent,
     LoaderComponent,
