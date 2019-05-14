0ea5e3e11d9e668c0916e5bf73003e9a25e9f16d
taras-d <taras.datsenko@gmail.com>
2019-05-14 12:47:25 +0300

    Remove obsolete code

--- src/app/core/app-start.service.ts
+++ src/app/core/app-start.service.ts
@@ -1,17 +1,9 @@
 import { Injectable } from '@angular/core';
 import { of } from 'rxjs';
 
-import { ApiService } from './api.service';
-
 @Injectable({ providedIn: 'root' })
 export class AppStartService {
-
-  constructor(private apiService: ApiService) {
-
-  }
-
   load(): Promise<any> {
     return of(null).toPromise();
   }
-
 }
